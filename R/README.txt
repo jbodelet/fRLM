6abdde69caa9915416af335cb656deb8e4279889
wiwh <wiwh404@gmail.com>
2023-11-27 10:48:28 +0100

    should work but not tested on actual data yet

---
+++ R/README.txt
@@ -0,0 +1,4 @@
+# Describes the different scripts
+
+fRLM_additive.R: additive version (with random effects) of 2-step fRLM
+fRLM_lpfr.R: one-step version (for now, not additive)
--- R/lpfr.R
+++ R/lpfr.R
@@ -1,5 +1,9 @@
-# Goldsmith method
-lpfr <- function(y, tobs, xobs, L = 4, K = 5, covariates = NULL, 
+#' Goldsmith method
+#' @param y: a vector of numeric values, the outcome, with vector elements corresponding to the units.
+#' @param tobs: a list of time of observations, with list elements corresponding to the units
+#' @param xobs: a list of exposures corresponding to the time observeations, with list elements corresponding to the units
+#' @param covariates: a matrix of covariates, with rows corresponding to the units.
+lpfr_stan <- function(y, tobs, xobs, L = 4, K = 5, covariates = NULL,
                            grid = seq(0,1, l = 150 ), compiled_file = NULL, ...){
   stopifnot(  K >= L ) # condition should be met (see 2010 Goldsmith paper)
   padding <- function(list_of_vec) t( sapply( list_of_vec, function(x) c( x, rep(0, Nmax - length(x) ) ) ) )
@@ -16,16 +20,15 @@ lpfr <- function(y, tobs, xobs, L = 4, K = 5, covariates = NULL,
   C <- cbind( rep(1, length(y) ), covariates )  # scalar predictors
   dat <- list( n = length(y), L = L, K = K, d = ncol(C), Nmax = Nmax, y = y, C = C, phi_mat = phi_mat,
                J = J, Nvec = Nvec, tobs = tobs_mat, xobs = xobs_mat )
-  if(is.null(compiled_file)){
-    fit <- rstan::stan( file = "./stan/lpfr.stan", data = dat, ... )  
-  }else{
-    fit <- rstan::sampling(compiled_file, data = dat, ...)            
-  }
-  out <- c( fit = fit, rstan::extract(fit ), L = L )
-  out$psi <- psi
-  out$Xhat <- t( sapply( apply(out$xi, 2, function(x) x %*% t(psi), simplify = F), colMeans ) )
-  class(out) <- "funcRegBayes"
-  return(out)
+
+  fit <- rstan::stan( file = compiled_file, data = dat, ... )
+  fit_list <- list(
+    fit = rstan::extract(fit),
+    fit_object = fit,
+    psi = psi,
+    Xhat = t( sapply( apply(out$xi, 2, function(x) x %*% t(psi), simplify = F), colMeans ) )
+  )
+  return(fit)
 }
 
 
